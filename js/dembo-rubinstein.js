2b56a9f7c2bd0eae0a97e3875e005d4c3cf2003c
reoge <reoge@mail.ru>
2021-08-12 22:16:13 +0300

    Implemented part 1 of dembo-rubinstein

--- js/dembo-rubinstein.js
+++ js/dembo-rubinstein.js
@@ -1,5 +1,4 @@
-import * as visual from "../lib/visual-2021.1.4.js";
-import * as util from "../lib/util-2021.1.4.js";
+import { util, visual } from "../lib/psychojs-2021.2.2.js";
 
 import { TaskPresenter, TaskView, Instruction } from "./general.js";
 
@@ -39,9 +38,9 @@ const instructions = [
 
 class SingleScale {
     constructor({ scaleDescrition, leftEnd, rightEnd }) {
-        this._scaleDescrition = scaleDescrition;
-        this._leftEnd = leftEnd;
-        this._rightEnd = rightEnd;
+        this.scaleDescrition = scaleDescrition;
+        this.leftEnd = leftEnd;
+        this.rightEnd = rightEnd;
     }
 }
 
@@ -79,64 +78,270 @@ const prebuildScales = [
 ];
 
 class DemboRubinsteinPresenter extends TaskPresenter {
-    constructor({ window }) {
-        const view = new DemboRubinsteinView({ window });
+    constructor({ window, screenSizeAdapter }) {
+        const view = new DemboRubinsteinView({ window, screenSizeAdapter });
         super({
             name: "DemboRubinstein",
             instructionsText: instructions,
             view: view,
         });
+
+        this._parts = ["prebuild", "participantGeneratesScales"];
+        this._part_idx = 0;
+        this._scalesSets = {
+            prebuild: Array.from(prebuildScales),
+            participantGeneratesScales: [],
+        };
+        this._currentScale = null;
+        this._trial_finished = false;
     }
 
     getTaskConditions() {
-        throw new Error(
-            `Method 'getTaskConditions()' must be implemented in ${this.name} class.`
-        );
+        // no information needed for InputProcessor
+        return;
     }
 
     nextStimulus() {
-        throw new Error(
-            `Method 'nextStimulus()' must be implemented in ${this.name} class.`
-        );
+        const part = this._parts[this._part_idx];
+        this._currentScale = this._scalesSets[part].pop();
+
+        const scaleDescription = this._currentScale.scaleDescrition;
+        const scaleEnds = {
+            leftEnd: this._currentScale.leftEnd,
+            rightEnd: this._currentScale.rightEnd,
+        };
+        this._view.setScale(scaleDescription, scaleEnds);
+        this._trial_finished = false;
     }
 
     checkInput(inputProcessor) {
-        throw new Error(
-            `Method 'checkInput(inputProcessor)' must be implemented in ${this.name} class.`
-        );
+        const inputData = inputProcessor.getData();
+
+        const scaleDescription = this._currentScale.scaleDescrition;
+        const leftEnd = this._currentScale.leftEnd;
+        const rightEnd = this._currentScale.rightEnd;
+        const part = this._parts[this._part_idx];
+
+        let attemptData = {
+            task: this.name,
+            part: part,
+            scaleDescription: scaleDescription,
+            scaleLeftEnd: leftEnd,
+            scaleRightEnd: rightEnd,
+            solved: 1,
+        };
+
+        attemptData = Object.assign(attemptData, inputData);
+
+        this._solutionAttemptsKeeper.saveAttempt(attemptData);
+
+        this._trial_finished = true;
     }
 
-    isTrialFinished(userInputProcessor) {
-        throw new Error(
-            `Method 'isTrialFinished(userInputProcessor)' must be implemented in ${this.name} class.`
-        );
+    addUnfinishedTrialData(userInputProcessor) {
+        if (this._trial_finished) {
+            return;
+        }
+    }
+
+    isTrialFinished() {
+        return this._trial_finished;
     }
 
     isTaskFinished() {
-        throw new Error(
-            `Method 'isTaskFinished()' must be implemented in ${this.name} class.`
-        );
+        return this._scalesSets.prebuild.length === 0;
+    }
+}
+
+class FilledScaleView {
+    constructor({ window, textSize, textPosition, textWidth }) {
+        console.log(textWidth);
+        this._scaleDescription = new visual.TextStim({
+            win: window,
+            color: new util.Color("black"),
+            height: textSize.description,
+            pos: textPosition.description,
+            text: "",
+            wrapWidth: textWidth.description,
+        });
+
+        this._leftEnd = new visual.TextStim({
+            win: window,
+            color: new util.Color("black"),
+            height: textSize.leftEnd,
+            pos: textPosition.leftEnd,
+            text: "",
+            wrapWidth: textWidth.leftEnd,
+        });
+
+        this._rightEnd = new visual.TextStim({
+            win: window,
+            color: new util.Color("black"),
+            height: textSize.rightEnd,
+            pos: textPosition.rightEnd,
+            text: "",
+            wrapWidth: textWidth.rightEnd,
+        });
+    }
+
+    setScale(scaleDescription, scaleEnds) {
+        this._scaleDescription.text = scaleDescription;
+        this._leftEnd.text = scaleEnds.leftEnd;
+        this._rightEnd.text = scaleEnds.rightEnd;
+    }
+
+    setAutoDraw(toShow) {
+        this._scaleDescription.setAutoDraw(toShow);
+        this._leftEnd.setAutoDraw(toShow);
+        this._rightEnd.setAutoDraw(toShow);
+    }
+}
+
+class EmptyScaleView {
+    constructor({ window, textSize, fieldPosition, fieldSize }) {
+        this._scaleDescription = new visual.TextBox({
+            win: window,
+            text: "[Напишите название качества]",
+            font: "Open Sans",
+            pos: fieldPosition.description,
+            letterHeight: textSize.description,
+            size: fieldSize.description,
+            color: "black",
+            colorSpace: "rgb",
+            fillColor: undefined,
+            borderColor: "black",
+            bold: true,
+            italic: false,
+            editable: true,
+            multiline: true,
+            anchor: "center",
+            depth: 0.0,
+            autofocus: false,
+        });
+
+        this._leftEnd = new visual.TextBox({
+            win: window,
+            name: "scaleDescription",
+            text: "[Напишите название низкого уровня]",
+            font: "Open Sans",
+            pos: fieldPosition.leftEnd,
+            letterHeight: textSize.leftEnd,
+            size: fieldSize.leftEnd,
+            color: "black",
+            colorSpace: "rgb",
+            fillColor: undefined,
+            borderColor: "black",
+            bold: true,
+            italic: false,
+            editable: true,
+            multiline: true,
+            anchor: "center",
+            depth: 0.0,
+            autofocus: false,
+        });
+
+        this._rightEnd = new visual.TextBox({
+            win: window,
+            name: "scaleDescription",
+            text: "[Напишите название высокого уровня]",
+            font: "Open Sans",
+            pos: fieldPosition.rightEnd,
+            letterHeight: textSize.rightEnd,
+            size: fieldSize.rightEnd,
+            color: "black",
+            colorSpace: "rgb",
+            fillColor: undefined,
+            borderColor: "black",
+            bold: true,
+            italic: false,
+            editable: true,
+            multiline: true,
+            anchor: "center",
+            depth: 0.0,
+            autofocus: false,
+        });
+    }
+
+    setScale() {
+        this._scaleDescription.reset();
+        this._leftEnd.reset();
+        this._rightEnd.reset();
+    }
+
+    setAutoDraw(toShow) {
+        this._scaleDescription.setAutoDraw(toShow);
+        this._leftEnd.setAutoDraw(toShow);
+        this._rightEnd.setAutoDraw(toShow);
     }
 }
 
 class DemboRubinsteinView extends TaskView {
-    constructor({ window }) {
+    constructor({ window, screenSizeAdapter }) {
         super({});
-        this.dummy = new visual.TextStim({
-            win: window,
-            color: new util.Color("black"),
-            height: 100,
-            text: 'Здесь должны были быть задания Дембо-Рубинштейн, но они пока не готовы.\nНажмите "q", чтобы посмотреть что из задач готово',
-            wrapWidth: window.size[0] * 0.8,
+        this._currentScale = null;
+
+        const fieldSize = {
+            description: screenSizeAdapter.rescaleElementSize([0.425, 0.1]),
+            leftEnd: screenSizeAdapter.rescaleElementSize([0.3, 0.15]),
+            rightEnd: screenSizeAdapter.rescaleElementSize([0.3, 0.15]),
+        };
+
+        const textSize = {
+            description: screenSizeAdapter.rescaleTextSize(0.05),
+            leftEnd: screenSizeAdapter.rescaleTextSize(0.05),
+            rightEnd: screenSizeAdapter.rescaleTextSize(0.05),
+        };
+
+        const fieldPosition = {
+            description: screenSizeAdapter.rescalePosition([0, 0.2]),
+            leftEnd: screenSizeAdapter.rescalePosition([-0.25, -0.05]),
+            rightEnd: screenSizeAdapter.rescalePosition([0.25, -0.05]),
+        };
+
+        const textPosition = {
+            description: screenSizeAdapter.rescalePosition([0, 0.2]),
+            leftEnd: screenSizeAdapter.rescalePosition([-0.25, 0]),
+            rightEnd: screenSizeAdapter.rescalePosition([0.25, 0]),
+        };
+
+        const textWidth = {
+            description: screenSizeAdapter.rescaleWrapWidth(0.5),
+            leftEnd: screenSizeAdapter.rescaleWrapWidth(0.25),
+            rightEnd: screenSizeAdapter.rescaleWrapWidth(0.25),
+        };
+
+        this._filledScale = new FilledScaleView({
+            window: window,
+            textSize: textSize,
+            textPosition: textPosition,
+            textWidth: textWidth,
+        });
+        this._emptyScale = new EmptyScaleView({
+            window: window,
+            textSize: textSize,
+            fieldPosition: fieldPosition,
+            fieldSize: fieldSize,
         });
     }
 
-    stop() {
-        this.dummy.setAutoDraw(false);
+    _isFilledScale(scaleDescription, scaleEnds) {
+        return scaleDescription !== null && scaleEnds !== null;
     }
 
-    draw() {
-        this.dummy.draw();
+    setScale(scaleDescription, scaleEnds) {
+        if (this._isFilledScale(scaleDescription, scaleEnds)) {
+            this._filledScale.setScale(scaleDescription, scaleEnds);
+            this._currentScale = this._filledScale;
+        } else {
+            this._emptyScale.setScale();
+            this._currentScale = this._emptyScale;
+        }
+    }
+
+    _setEmptyEnds() {}
+
+    setAutoDraw(toShow) {
+        this._currentScale.setAutoDraw(toShow);
     }
 }
 
