a29d029e5264903e8bbe9f8e3a91d47cb4566f5c
reoge <reoge@mail.ru>
2021-08-27 23:16:15 +0300

    Added boilerplate for task

--- js/luchins.js
+++ js/luchins.js
@@ -1,23 +1,76 @@
 import { util, visual } from "../lib/psychojs-2021.2.2.js";
 
-class LuchinsView {
-    constructor({ window }) {
-        this.dummy = new visual.TextStim({
-            win: window,
-            color: new util.Color("black"),
-            height: 100,
-            text: 'Здесь должны были быть задания Лачинсов, но они пока не готовы.\nНажмите "q", чтобы посмотреть что из задач готово',
-            wrapWidth: window.size[0] * 0.8,
-        });
+import { TaskPresenter, TaskView, Instruction } from "./general.js";
+
+const instruction = ``;
+
+class LuchinsPresenter extends TaskPresenter {
+    constructor({ window, screenSizeAdapter, startTime }) {
+        const instructions = [new Instruction(instruction)];
+        const view = new LuchinsView({ window, screenSizeAdapter, startTime });
+        super({ name: "Luchins", instructionsText: instructions, view: view });
+
+        this._task = null;
+        this._tasks = [];
     }
 
-    stop() {
-        this.dummy.setAutoDraw(false);
+    getTaskConditions() {
+        throw new Error(
+            `Method 'getTaskConditions()' must be implemented in ${this.name} class.`
+        );
     }
 
-    draw() {
-        this.dummy.draw();
+    nextStimulus() {
+        throw new Error(
+            `Method 'nextStimulus()' must be implemented in ${this.name} class.`
+        );
+    }
+
+    checkInput(userInputProcessor) {
+        throw new Error(
+            `Method 'checkInput(userInputProcessor)' must be implemented in ${this.name} class.`
+        );
+    }
+
+    isToSkipInstruction() {
+        return false;
+    }
+
+    isTrialFinished() {
+        throw new Error(
+            `Method 'isTrialFinished()' must be implemented in ${this.name} class.`
+        );
+    }
+
+    isTaskFinished() {
+        throw new Error(
+            `Method 'isTaskFinished()' must be implemented in ${this.name} class.`
+        );
     }
 }
 
-export { LuchinsView as Luchins };
+class LuchinsView extends TaskView {
+    constructor({ window, screenSizeAdapter, startTime }) {
+        super({ startTime });
+
+        this._task = new visual.TextStim({
+            win: window,
+            text: "",
+            color: "black",
+            pos: screenSizeAdapter.rescalePosition([0, 0.2]),
+            height: screenSizeAdapter.rescaleTextSize(0.15),
+            autoDraw: false,
+            bold: true,
+        });
+    }
+
+    setLuchinsTask(task) {
+        this._task.text = task;
+    }
+
+    setAutoDraw(toShow) {
+        this._task.setAutoDraw(toShow);
+    }
+}
+
+export { LuchinsPresenter as Luchins };
